548af42f06b6092e4951a074f06fb766e2e91bf3
Oliver Nguyen <oliver.nguyen@prosiebensat1.com>
2022-06-10 11:32:39 +0200

    added possible implementation in Element.ts

--- src/screenplay/web/questions/Element.ts
+++ src/screenplay/web/questions/Element.ts
@@ -1,4 +1,5 @@
 import { Actor, Question } from '@testla/screenplay';
+import { expect } from '@playwright/test';
 import { SelectorOptions } from '../types';
 import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
 
@@ -6,20 +7,55 @@ import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
  * Question Class. Get a specified state for a selector like visible or enabled.
  */
 export class Element extends Question<boolean> {
-    private mode: 'visible' | 'enabled';
+    // if true -> Element.not was called, answeredBy has to check for opposite
+    private static checkMode: 'is' | 'not';
 
-    private constructor(mode: 'visible' | 'enabled', private selector: string, private options?: SelectorOptions & { wait?: boolean }) {
+    private constructor(private mode: 'visible' | 'enabled', private selector: string, private options?: SelectorOptions & { wait?: boolean }) {
         super();
         this.mode = mode;
     }
 
-    public answeredBy(actor: Actor): Promise<boolean> {
+    public async answeredBy(actor: Actor): Promise<boolean> {
         if (this.mode === 'visible') {
-            return BrowseTheWeb.as(actor).isVisible(this.selector, this.options);
+            if (Element.checkMode === 'is') {
+                // .is was called -> perform positive check
+                expect(await BrowseTheWeb.as(actor).isVisible(this.selector, this.options)).toBe(true);
+                return true; // have to return something, if the question fails there will be an exception anyways
+            }
+            if (Element.checkMode === 'not') {
+                // .not was called -> perform negtive check
+                expect(await BrowseTheWeb.as(actor).isVisible(this.selector, this.options)).toBe(false);
+                return true; // have to return something, if the question fails there will be an exception anyways
+            }
         } if (this.mode === 'enabled') {
-            return BrowseTheWeb.as(actor).isEnabled(this.selector, this.options);
+            if (Element.checkMode === 'is') {
+                // .is was called -> perform positive check
+                expect(await BrowseTheWeb.as(actor).isEnabled(this.selector, this.options)).toBe(true);
+                return true;
+            }
+            if (Element.checkMode === 'not') {
+                // .not was called -> perform negtive check
+                expect(await BrowseTheWeb.as(actor).isEnabled(this.selector, this.options)).toBe(false);
+                return true;
+            }
         }
-        throw new Error('Unknown mode');
+        throw new Error('Unknown mode: Element.answeredBy');
+    }
+
+    /**
+     * make the Question check for the positive.
+     */
+    static get is() {
+        Element.checkMode = 'is';
+        return Element;
+    }
+
+    /**
+     * make the Question check for the negative.
+     */
+    static get not() {
+        Element.checkMode = 'not';
+        return Element;
     }
 
     /**
@@ -32,8 +68,7 @@ export class Element extends Question<boolean> {
         const newOptions = { ...options };
         delete newOptions.wait;
 
-        // it is possible to expect an instant availability
-        // for that the option wait must explicitely set to false
+        // it is possible to expect an instant availability -> for that the option wait must explicitely set to false
         // the default to 1ms is a defacto instant
         if (options?.wait === false) { newOptions.timeout = 1; }
 
