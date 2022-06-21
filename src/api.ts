aeb1ec4ba83f8136cda99d30418231848d0ad714
Oliver Nguyen <oliver.nguyen@prosiebensat1.com>
2022-06-21 11:37:22 +0200

    updated imports and exports

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import { UseAPI } from './screenplay/api/abilities/UseAPI';
+import { Delete } from './screenplay/api/actions/Delete';
+import { Get } from './screenplay/api/actions/Get';
+import { Head } from './screenplay/api/actions/Head';
+import { Patch } from './screenplay/api/actions/Patch';
+import { Post } from './screenplay/api/actions/Post';
+import { Put } from './screenplay/api/actions/Put';
+import { Response } from './screenplay/api/questions/Response';
+
+export {
+    UseAPI, Delete, Get, Head, Patch, Post, Put, Response,
+};
---
+++ src/web.ts
@@ -0,0 +1,21 @@
+import { BrowseTheWeb } from './screenplay/web/abilities/BrowseTheWeb';
+import { Add } from './screenplay/web/actions/Add';
+import { Check } from './screenplay/web/actions/Check';
+import { Clear } from './screenplay/web/actions/Clear';
+import { Click } from './screenplay/web/actions/Click';
+import { DoubleClick } from './screenplay/web/actions/DoubleClick';
+import { DragAndDrop } from './screenplay/web/actions/DragAndDrop';
+import { Fill } from './screenplay/web/actions/Fill';
+import { Get } from './screenplay/web/actions/Get';
+import { Hover } from './screenplay/web/actions/Hover';
+import { Navigate } from './screenplay/web/actions/Navigate';
+import { Press } from './screenplay/web/actions/Press';
+import { Remove } from './screenplay/web/actions/Remove';
+import { Set } from './screenplay/web/actions/Set';
+import { Type } from './screenplay/web/actions/Type';
+import { Wait } from './screenplay/web/actions/Wait';
+import { Element } from './screenplay/web/questions/Element';
+
+export {
+    BrowseTheWeb, Check, Wait, Hover, Navigate, Fill, Press, DragAndDrop, Click, DoubleClick, Type, Element, Get, Add, Clear, Remove, Set,
+};
