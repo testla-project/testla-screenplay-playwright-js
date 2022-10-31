4a395db986aaadf7e144a73a00b9bfec6365940f
Oliver Nguyen <oliver.nguyen@prosiebensat1.com>
2022-10-31 12:51:35 +0100

    added api tests

--- __tests__/api.spec.ts
+++ __tests__/api.spec.ts
@@ -7,6 +7,7 @@ import { Patch } from '../src/api/actions/Patch';
 import { Put } from '../src/api/actions/Put';
 import { Head } from '../src/api/actions/Head';
 import { Delete } from '../src/api/actions/Delete';
+import { Response } from '../src/api/questions/Response';
 
 type MyActors = {
     actor: Actor;
@@ -66,6 +67,23 @@ test.describe('Testing screenplay-playwright-js web module', () => {
         );
         expect(response.status).toBe(200);
         expect(response.body).toStrictEqual(data);
+
+        const responseWithHeaders = await actor.attemptsTo(
+            Patch.to('https://jsonplaceholder.typicode.com/posts/1').withData(data)
+                .withHeaders({
+                    'Content-type': 'text/plain; charset=UTF-8',
+                }),
+        );
+        expect(responseWithHeaders.status).toBe(200);
+        expect(responseWithHeaders.body).toStrictEqual({
+            userId: 1,
+            id: 1,
+            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+            body: 'quia et suscipit\n'
+              + 'suscipit recusandae consequuntur expedita et cum\n'
+              + 'reprehenderit molestiae ut ut quas totam\n'
+              + 'nostrum rerum est autem sunt rem eveniet architecto',
+        });
     });
 
     test('DELETE', async ({ actor }) => {
@@ -73,5 +91,47 @@ test.describe('Testing screenplay-playwright-js web module', () => {
             Delete.from('https://jsonplaceholder.typicode.com/posts/1'),
         );
         expect(response.status).toBe(200);
+    });
+
+    test('HEAD', async ({ actor }) => {
+        const response = await actor.attemptsTo(
+            Head.from('https://jsonplaceholder.typicode.com/posts/1'),
+        );
+        expect(response.status).toBe(200);
+        expect(response.body).toBeNull();
+    });
+
+    test.only('Response (Question)', async ({ actor }) => {
+        const response = await actor.attemptsTo(
+            Get.from('http://zippopotam.us/us/90210').withResponseBodyFormat('text'),
+        );
+
+        await actor.asks(
+            Response.has.statusCode(response, 200),
+        );
+
+        let res = false;
+        try {
+            await actor.asks(
+                Response.has.statusCode(response, 404),
+            );
+        } catch (error) {
+            res = true;
+        }
+        expect(res).toBeTruthy();
+
+        let res2 = false;
+        try {
+            await actor.asks(
+                Response.hasNot.statusCode(response, 200),
+            );
+        } catch (error) {
+            res2 = true;
+        }
+        expect(res2).toBeTruthy();
+
+        await actor.asks(
+            Response.hasNot.statusCode(response, 404),
+        );
     });
 });
