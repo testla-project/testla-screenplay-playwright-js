fe5aaf93a3f2f91a81148cfb4f16b4225bffab16
Brückner, Kenji <Kenji.Brueckner@prosiebensat1.com>
2023-12-20 14:18:34 +0100

    added tests

---
+++ __tests__/aliasing.spec.ts
@@ -0,0 +1,51 @@
+import { expect, test as base } from '@playwright/test';
+import { Actor } from '@testla/screenplay';
+import { UseAPI } from '../src/api/abilities/UseAPI';
+import { Get } from '../src/api/actions/Get';
+import { BrowseTheWeb, Navigate } from '../src/web';
+
+const ALIAS = 'alias';
+
+type MyActors = {
+    actor: Actor;
+};
+
+const test = base.extend<MyActors>({
+    actor: async ({ request, browser }, use) => {
+        const context = await browser.newContext();
+        const page1 = await context.newPage();
+        const page2 = await context.newPage();
+        const actor = Actor.named('TestActor')
+            .can(UseAPI.using(request))
+            .can(UseAPI.using(request).withAlias(ALIAS))
+            .can(BrowseTheWeb.using(page1))
+            .can(BrowseTheWeb.using(page2).withAlias(ALIAS));
+        await use(actor);
+    },
+});
+
+test.describe('Testing screenplay-playwright-js ability aliasing', () => {
+    test('UseAPI', async ({ actor }) => {
+        const response = await actor.attemptsTo(
+            Get.from('http://zippopotam.us/us/90210').withResponseBodyFormat('text'),
+        );
+        expect(response.status).toBe(200);
+
+        const response2 = await actor.attemptsTo(
+            Get.from('http://zippopotam.us/us/90210').withResponseBodyFormat('text').withAbilityAlias(ALIAS),
+        );
+        expect(response2.status).toBe(200);
+    });
+
+    test('BrowseTheWeb', async ({ actor }) => {
+        await actor.attemptsTo(
+            Navigate.to('https://google.de'),
+        );
+        await expect(BrowseTheWeb.as(actor).getPage()).toHaveURL('https://www.google.de');
+
+        await actor.attemptsTo(
+            Navigate.to('https://google.com').withAbilityAlias(ALIAS),
+        );
+        await expect(BrowseTheWeb.as(actor, ALIAS).getPage()).toHaveURL('https://www.google.com');
+    });
+});
