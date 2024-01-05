ba00666172b17dfc133cceaf90acdf7a8f231577
Brückner, Kenji <Kenji.Brueckner@prosiebensat1.com>
2024-01-05 20:17:28 +0100

    draft integration

--- src/api/types.ts
+++ src/api/types.ts
@@ -5,13 +5,17 @@ export type Headers = {
     [key: string]: string;
 };
 
-export type ResponseBodyType = Buffer | object | string | null;
+export type ResponseBodyType = unknown;
+// export type ResponseBodyType = Buffer | object | string | null;
 
 /**
- * Response type which is returned from any request
+ * Response type which is returned from any request.
+ *
+ * @param T (optional) Response body type - usually one out of `Buffer` | `object` | `string` | `null`, defaults to `unknown`.
  */
-export type Response = {
-    body: ResponseBodyType;
+export type Response<T = ResponseBodyType> = {
+    // export type Response<T = unknown> = {
+    body: T;
     status: number;
     headers: Headers;
     duration: number;
