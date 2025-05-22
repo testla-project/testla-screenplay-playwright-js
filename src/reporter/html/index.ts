11b0198673ac21373613000049394f534e9dd62e
Kenji Brückner <kenji.brueckner@gmail.com>
2025-05-22 15:03:53 +0200

    saving status quo

--- src/reporter/html/index.ts
+++ src/reporter/html/index.ts
@@ -1,6 +1,10 @@
 import { FullResult } from '@playwright/test/reporter';
 import { STRUCTURED_LOGS_ENVVAR_NAME } from '@testla/screenplay';
-import { appendFileSync, cpSync, existsSync, mkdirSync, rm } from 'fs';
+import {
+    cpSync, existsSync,
+    rmSync,
+    writeFileSync,
+} from 'fs';
 import JsonReporter from '../json';
 
 class HtmlReporter extends JsonReporter {
@@ -8,25 +12,21 @@ class HtmlReporter extends JsonReporter {
 
     constructor(config: any) {
         super(config);
-        this.outputDir = config.configDir || 'screenplay-report';
+        this.outputDir = `${config.configDir}/${config.outputDir || 'screenplay-report'}`;
     }
 
     protected write(content: string) {
         if (this.outputDir) {
-            appendFileSync(`${this.outputDir}/report.js`, content);
+            writeFileSync(`${this.outputDir}/report.js`, content);
         }
     }
 
     onBegin() {
         if (this.outputDir && existsSync(this.outputDir)) {
-            rm(this.outputDir, { recursive: true, force: true }, (err) => {
-                if (err) {
-                    throw err;
-                }
-            });
-            // mkdirSync(this.outputDir);
-            cpSync('src', this.outputDir, { recursive: true });
+            rmSync(this.outputDir, { recursive: true, force: true });
         }
+        // mkdirSync(this.outputDir);
+        cpSync(`${__dirname}/src`, this.outputDir, { recursive: true });
 
         process.env[STRUCTURED_LOGS_ENVVAR_NAME] = 'true';
     }
@@ -36,7 +36,7 @@ class HtmlReporter extends JsonReporter {
             ...result,
             executions: this.executions,
         };
-        this.write(JSON.stringify(report));
+        this.write(`const report = ${JSON.stringify(report)}`);
     }
 }
 
--- src/reporter/html/src/scripts.js
+++ src/reporter/html/src/scripts.js
@@ -23,9 +23,59 @@ const getStatusIcon = (status) => {
 const renderExecutions = (executions) => {
     const report = document.getElementById('report');
     const list = document.createElement('ul');
+    const groupedExecutions = new Map();
     executions.forEach((execution) => {
+        if (groupedExecutions.has(execution.suite)) {
+            groupedExecutions.get(execution.suite).push(execution);
+        } else {
+            groupedExecutions.set(execution.suite, [execution]);
+        }
+    });
+    groupedExecutions.forEach((executions, suite) => {
         const item = document.createElement('li');
-        item.innerText = getStatusIcon(execution.status) + ' ' + execution.title + ' (' + execution.duration + 'ms)';
+        item.classList.add('collapsible');
+        const header = document.createElement('header');
+        header.onclick = () => {
+            item.classList.toggle('collapsed');
+        };
+        const areaTitle = document.createElement('span');
+        areaTitle.innerText = suite;
+        const toggleIcon = document.createElement('span');
+        toggleIcon.classList.add('icon');
+        toggleIcon.innerText = '▼';
+        header.appendChild(toggleIcon);
+        header.appendChild(areaTitle);
+        item.appendChild(header);
+        const sublist = document.createElement('ul');
+        sublist.classList.add('content');
+        executions.forEach((execution) => {
+            const subitem = document.createElement('li');
+            const subitemContent = document.createElement('div');
+            subitemContent.classList.add('content');
+            const statusIcon = document.createElement('div');
+            statusIcon.classList.add('status-icon');
+            statusIcon.innerText = getStatusIcon(execution.status);
+            subitemContent.appendChild(statusIcon);
+            const title = document.createElement('div');
+            title.classList.add('title');
+            title.innerText = execution.title;
+            subitemContent.appendChild(title);
+            const project = document.createElement('div');
+            project.classList.add('project');
+            const projectInnerText = document.createElement('div');
+            projectInnerText.classList.add('inner');
+            projectInnerText.innerText = execution.project;
+            project.appendChild(projectInnerText);
+            subitemContent.appendChild(project);
+            const duration = document.createElement('div');
+            duration.classList.add('duration');
+            duration.innerText = execution.duration + 'ms';
+            subitemContent.appendChild(duration);
+            // subitemContent.innerText = getStatusIcon(execution.status) + ' ' + execution.title + ' (' + execution.duration + 'ms)';
+            subitem.appendChild(subitemContent);
+            sublist.appendChild(subitem);
+        });
+        item.appendChild(sublist);
         list.appendChild(item);
     });
     report.innerHTML = '';
