100d7eb779e641f8b1fb0c2bf6d3074881807506
Kenji Brückner <kenji.brueckner@gmail.com>
2025-08-28 12:40:48 +0200

    updated web / action / draganddrop

--- src/web/actions/DragAndDrop/DragAndDrop.ts
+++ src/web/actions/DragAndDrop/DragAndDrop.ts
@@ -1,7 +1,7 @@
 import { Actor } from '@testla/screenplay';
-import { Selector, SelectorOptions } from '../types';
-import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
-import { FrameEnabledAction } from '../templates/FrameEnabledAction';
+import { Selector, SelectorOptions } from '../../types';
+import { BrowseTheWeb } from '../../abilities/BrowseTheWeb';
+import { FrameEnabledAction } from '../../templates/FrameEnabledAction';
 
 /**
  * Action Class. DragAndDrop an element specified by a selector string and drop it on an element specified by another
